7c77c945b9dd3d57503e74d2d52df6a1d74beff6
therareaniket <aniketbhatti.rarepixelsdesign@gmail.com>
2025-09-03 14:18:11 +0530

    build 🛠️: Set the Globles for Website + Add Fonts + Add ShadCN + Add Header & Footer files

---
+++ compliance-web/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+// "use client"
+import React from 'react'
+
+export function Footer() {
+    return (
+        <footer>
+            <h1>I am Footer</h1>
+        </footer>
+    )
+}
---
+++ compliance-web/src/components/Header.tsx
@@ -0,0 +1,10 @@
+// "use client"
+import React from 'react'
+
+export function Header() {
+    return (
+        <header className="container">
+            <h1 className='text-3xl font-bold'>I am Header</h1>
+        </header>
+    )
+}
