7c87ffda1a0e66070a63e7261e77c7d21b0e6bad
therareaniket <aniketbhatti.rarepixelsdesign@gmail.com>
2025-09-03 16:08:44 +0530

    build 🛠️: Navbar Logo Set

--- compliance-web/src/components/Header.tsx
+++ compliance-web/src/components/Header.tsx
@@ -1,10 +1,20 @@
 // "use client"
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export function Header() {
     return (
-        <header className="container">
-            <h1 className='text-3xl font-bold'>I am Header</h1>
+        <header>
+            <div className="container">
+                <div className="navbar-wrapper">
+                    <div className="navbar-logo">
+                        <Link href='/'>
+                            <Image src='/images/Header/logo-header.svg' alt='logo' width={239} height={34} priority={true}></Image>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </header>
     )
 }
