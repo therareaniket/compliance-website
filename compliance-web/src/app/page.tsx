17cfd40abad4cd6d0bb921fe7315fa7c5f807e65
therareaniket <aniketbhatti.rarepixelsdesign@gmail.com>
2025-09-08 17:36:06 +0530

    feat 🚀: Homepage with btn globles

--- compliance-web/src/app/page.tsx
+++ compliance-web/src/app/page.tsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
 import AboutUs from "./About/page";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-        <main>Hello I am Main</main>
+        <>
+            <section className="hm-hero">
+                <div className="container">
+                    <div className="hm-hero-content">
+                        <h1 className="text-center">Stay Ahead with<br />Smarter <span>Compliance</span> Monitoring</h1>
+
+                        <p className="text-center text-20 text-grey">Stay ahead of audits with automated compliance scanning that detects gaps across HIPAA, GDPR, 21 CFR Part 11, and more. Our platform centralizes reporting, reduces manual effort, and shortens audit preparation.</p>
+
+                        <div className="hm-hero-btns">
+                            <Link href='#' title="Get Started" className="btn-primary btn-padding text-md text-18 site-radius-10">Get Started</Link>
+
+                            <Link href='#' title="Get a Quick Demo" className="btn-white btn-padding text-md text-18 site-radius-10">Get a Quick Demo</Link>  
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     </div>
   );
 }
