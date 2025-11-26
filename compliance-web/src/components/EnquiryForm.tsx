cf27c0f0e25386aeb1a9784c079ec0551c52b597
therareaniket <aniketbhatti.rarepixelsdesign@gmail.com>
2025-11-26 19:10:55 +0530

    fix 🐛: DhatuCompy Logo Change in Footer

--- compliance-web/src/components/EnquiryForm.tsx
+++ compliance-web/src/components/EnquiryForm.tsx
@@ -1,39 +1,3 @@
-// import React from 'react'
-
-// export default function EnquiryForm() {
-//     return (
-//         <>
-//             <form action="" method="post" aria-label="Enquiry form">
-//                 <div className="form-row">
-//                     <div className="form-col">
-//                         <label htmlFor="enq-name" className="text-18">Name<sup className="text-18">*</sup></label>
-//                         <input id="enq-name" name="name" placeholder="Enter your name" className="site-radius-10 text-20" type="text" required />
-//                     </div>
-
-//                     <div className="form-col">
-//                         <label htmlFor="enq-email" className="text-18">Email<sup className="text-18">*</sup></label>
-//                         <input id="enq-email" name="email" placeholder="Enter your mail" className="site-radius-10 text-20" type="email" required />
-//                     </div>
-
-//                     <div className="form-col">
-//                         <label htmlFor="enq-org" className="text-18">Organization<sup className="text-18">*</sup></label>
-//                         <input id="enq-org" name="organization" placeholder="Enter your organization" className="site-radius-10 text-20" type="text" required />
-//                     </div>
-
-//                     <div className="form-col">
-//                         <label htmlFor="enq-message" className="text-18">Message</label>
-//                         <textarea rows={6} id="enq-message" name="message" placeholder="Enter your message" className="site-radius-10 text-20" />
-//                     </div>
-//                 </div>
-
-//                 <button type="submit" className="text-md btn-padding btn-primary site-radius-10">Let’s Connect</button>
-//             </form>
-//         </>
-//     );
-// }
-
-
-
 // APP ROUTER CODE
 
 'use client';
--- compliance-web/src/components/Footer.tsx
+++ compliance-web/src/components/Footer.tsx
@@ -70,61 +70,61 @@ export async function Footer() {
                                 <Link href={ctahome.ctaLink3?.url as string} title={ctahome.ctaLink3?.title} className="btn-padding btn-white text-md text-18 site-radius-10">{ctahome.ctaLink3?.title}</Link>
                             </div>
                         </div>
-                    </section>
+            </section>
         
-        <footer className='footer'>
-            <div className="container">
-                <div className="footer-wrapper">
-                    <div className="comp-intro">
-                        <Image src='/images/footer/logo-footer.svg' alt='logo' width={217} height={31} priority={true} draggable={false} style={{ marginBottom: '20px' }}></Image>
+            <footer className='footer'>
+                <div className="container">
+                    <div className="footer-wrapper">
+                        <div className="comp-intro">
+                            <Image src='/images/footer/DhatuComply-footer-logo.svg' alt='logo' width={217} height={31} priority={true} draggable={false} style={{ marginBottom: '20px' }}></Image>
 
-                        <p>Smarter Monitoring & Reporting for Simplified Compliance</p>
+                            <p>Smarter Monitoring & Reporting for Simplified Compliance</p>
+                        </div>
+                        <div className="footer-links">
+                            <div className='quick-links text-md'>
+                                <p className='text-16 text-md'>Quick Links</p>
+                                <ul>
+                                    <li><Link href="/About" title='' className='text-16 text-rg'>About</Link></li>
+                                    <li><Link href="/UserAccess" title='' className='text-16 text-rg'>User & Access</Link></li>
+                                    <li><Link href="/Compliance" title='' className='text-16 text-rg'>Compliance</Link></li>
+                                    <li><Link href="/Contact" title='' className='text-16 text-rg'>Contact</Link></li>
+                                </ul>
+                            </div>
+
+                            <div className='quick-links legal-info text-md'>
+                                <p className='text-16 text-md'>Legal Informations</p>
+                                <ul>
+                                    <li><Link href="/PrivacyPolicy" title='' className='text-16 text-rg'>Privacy & Policy</Link></li>
+                                    <li><Link href="/TermsandCondition" title='' className='text-16 text-rg'>Terms & Conditions</Link></li>
+                                </ul>
+                            </div>
+
+                            <div className='quick-links contact-info  text-md'>
+                                <p className='text-16 text-md'>Contact Info</p>
+                                <ul>
+                                    <li className='email-info'>
+                                        <Image src='/images/footer/foot-email.svg' alt='email-svg' width={16} height={16} priority={true}></Image>
+                                        <Link href="mailto:enquiry@dhatucomply.com" title='' className='text-16 text-rg'>{contact.email}</Link>
+                                    </li>
+
+                                    <li className='email-info mobile-info'>
+                                        <Image src='/images/footer/foot-call.svg' alt='call-svg' width={16} height={16} priority={true}></Image>
+                                        <Link href="#" title='' className='text-16 text-rg'>{contact.phoneNumber}</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                    <div className="footer-links">
-                        <div className='quick-links text-md'>
-                            <p className='text-16 text-md'>Quick Links</p>
-                            <ul>
-                                <li><Link href="/About" title='' className='text-16 text-rg'>About</Link></li>
-                                <li><Link href="/UserAccess" title='' className='text-16 text-rg'>User & Access</Link></li>
-                                <li><Link href="/Compliance" title='' className='text-16 text-rg'>Compliance</Link></li>
-                                <li><Link href="/Contact" title='' className='text-16 text-rg'>Contact</Link></li>
-                            </ul>
-                        </div>
 
-                        <div className='quick-links legal-info text-md'>
-                            <p className='text-16 text-md'>Legal Informations</p>
-                            <ul>
-                                <li><Link href="/PrivacyPolicy" title='' className='text-16 text-rg'>Privacy & Policy</Link></li>
-                                <li><Link href="/TermsandCondition" title='' className='text-16 text-rg'>Terms & Conditions</Link></li>
-                            </ul>
-                        </div>
-
-                        <div className='quick-links contact-info  text-md'>
-                            <p className='text-16 text-md'>Contact Info</p>
-                            <ul>
-                                <li className='email-info'>
-                                    <Image src='/images/footer/foot-email.svg' alt='email-svg' width={16} height={16} priority={true}></Image>
-                                    <Link href="mailto:enquiry@dhatucomply.com" title='' className='text-16 text-rg'>{contact.email}</Link>
-                                </li>
-
-                                <li className='email-info mobile-info'>
-                                    <Image src='/images/footer/foot-call.svg' alt='call-svg' width={16} height={16} priority={true}></Image>
-                                    <Link href="#" title='' className='text-16 text-rg'>{contact.phoneNumber}</Link>
-                                </li>
-                            </ul>
+                    <div className="copyright">
+                        <div className="copyright-box">
+                            <p className='text-14 text-rg text-grey'>Powered by DFOLDS</p>
+                            <p className='text-14 text-rg text-grey'>DhatuComply 2025.1.1</p>
+                            <p className='text-14 text-rg text-grey'>Copyright ©{new Date().getFullYear()}, all rights reserved. </p>
                         </div>
                     </div>
                 </div>
-
-                <div className="copyright">
-                    <div className="copyright-box">
-                        <p className='text-14 text-rg text-grey'>Powered by DFOLDS</p>
-                        <p className='text-14 text-rg text-grey'>DhatuComply 2025.1.1</p>
-                        <p className='text-14 text-rg text-grey'>Copyright ©{new Date().getFullYear()}, all rights reserved. </p>
-                    </div>
-                </div>
-            </div>
-        </footer>
+            </footer>
 
         </>
     )
