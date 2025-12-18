bb3ba77eef90cc70ca8286cf3e3901d8042bf676
therareaniket <aniketbhatti.rarepixelsdesign@gmail.com>
2025-12-18 18:21:47 +0530

    test 🧪:  Removed Testimonial Animation of framer-motion.

--- compliance-web/src/components/TestiHome.tsx
+++ compliance-web/src/components/TestiHome.tsx
@@ -11,13 +11,13 @@ type TestimProps = {
 
 export default function TestimHome({ testim_title, testim_subTitle } : TestimProps) {
 
-    const TestiAnimation = (typeof window !== "undefined" && window.innerWidth >= 1100) ? {
-        initialHead: { opacity: 0, x: -100},
-        animateHead: { opacity: 1, x: 0, transition: { duration: 1, delay: 1 }},
-    } : {
-        initialHead: { opacity: 1, x: 0},
-        animateHead: { opacity: 1, x: 0,},
-    }
+    // const TestiAnimation = (typeof window !== "undefined" && window.innerWidth >= 1100) ? {
+    //     initialHead: { opacity: 0, x: -100},
+    //     animateHead: { opacity: 1, x: 0, transition: { duration: 1, delay: 1 }},
+    // } : {
+    //     initialHead: { opacity: 1, x: 0},
+    //     animateHead: { opacity: 1, x: 0,},
+    // }
 
     return(
         <>
@@ -25,11 +25,11 @@ export default function TestimHome({ testim_title, testim_subTitle } : TestimPro
                 <div className="container">
                     <div className="clientale-wrapper">
                         <div className="trusted-leaders site-radius-10">
-                            <motion.div className="trst-lead-content" variants={TestiAnimation} initial='initialHead' whileInView='animateHead' viewport={{ once: true, amount: 0.6 }}>
+                            <div className="trst-lead-content">
                                 <h2 className="h3">{testim_title}</h2>
 
                                 <p className="text-20 text-grey">{testim_subTitle}</p>
-                            </motion.div>
+                            </div>
 
                             <Image src='/images/homepage/clientale-quotes.webp' alt="quotes" width={588} height={410} priority={false} style={{ opacity: '0.2' }} draggable={false} className="testi-quotes"></Image>
                         </div>
